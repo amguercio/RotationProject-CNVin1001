7d050ee7456730d6e8d3fcf243e773bb137fd2f6
amguercio <45185200+amguercio@users.noreply.github.com>
2018-11-19 15:20:39 -0800

    Create picking_genomes.sh

---
+++ picking_genomes.sh
@@ -0,0 +1,12 @@
+#picking_genomes.sh
+
+###Script to pick 50 genomes randomly from 1001 genomes project in arabidopsis
+
+#make a list of the 1001 genome names
+
+#pick 50
+
+#download them to the cluster
+
+#also download the reference genome from tair
+
