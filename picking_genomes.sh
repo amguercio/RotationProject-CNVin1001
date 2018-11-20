997869a919be8275cf306456ebc69424ebb89444
amguercio <45185200+amguercio@users.noreply.github.com>
2018-11-19 22:03:13 -0800

    Update picking_genomes.sh

--- picking_genomes.sh
+++ picking_genomes.sh
@@ -1,12 +1,31 @@
 #picking_genomes.sh
 
-###Script to pick 50 genomes randomly from 1001 genomes project in arabidopsis
+##Script to pick 50 genomes randomly from 1001 genomes project in arabidopsis
 
-#make a list of the 1001 genome names
+## amguercio (Angelica Guercio) 2018
 
-#pick 50
+
+
+###Get Accession Info
+
+#download the table of all available sequenced accessions from the 1001 genomes project
+#at https://1001genomes.org/accessions.html
+
+
+###Pick 50 Random Accessions (for testing purposes)
+
+#shuf -n numberoflines_topick inputfile.csv > output.txt
+
+shuf -n 50 input > output
+
+#or if on my comp with zsh + homebrew -- brew install coreutils
+gshuf -n 50 seqdata_list1001.csv > testtwo.csv
+
 
 #download them to the cluster
+
+wget
+
 
 #also download the reference genome from tair
 
