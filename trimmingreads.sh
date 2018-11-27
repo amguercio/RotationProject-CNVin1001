e16a9c2e4b31adff357a631b5a9676b8749d2bf1
amguercio <45185200+amguercio@users.noreply.github.com>
2018-11-27 15:21:15 -0800

    Update trimmingreads.sh

--- trimmingreads.sh
+++ trimmingreads.sh
@@ -11,17 +11,17 @@
 module load trimmomatic/0.33
 
 
-cat runIDs_fordl.txt | while read line; do
+cat testrunID.txt | while read line; do
   NAME=$line
-  java -jar /opt/linux/centos/7.x/x86_64/pkgs/trimmomatic/0.33/bin/trimmomatic.jar PE -threads 4\
-  "$NAME"_1.fastq \
-  "$NAME"_2.fastq \
+  java -jar /software/trimmomatic/0.33/static/trimmomatic-0.33.jar PE -threads 4 \
+  "$NAME"_1.fastq.gz \
+  "$NAME"_2.fastq.gz \
   "$NAME"_forward_paired.fq.gz \
   "$NAME"_forward_unpaired.fq.gz \
   "$NAME"_reverse_paired.fq.gz \
   "$NAME"_reverse_unpaired.fq.gz \
-  ILLUMINACLIP:/opt/linux/centos/7.x/x86_64/pkgs/trimmomatic/0.33/adapters\
-  /TruSeq3-SE.fa:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:25
+  ILLUMINACLIP:/software/trimmomatic/0.33/static/adapters/TruSeq3-SE.fa:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:25
 done
+
 
 
