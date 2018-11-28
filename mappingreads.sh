874a7dc57b3727ede59d62e896d425d1a9e1cbc3
amguercio <45185200+amguercio@users.noreply.github.com>
2018-11-28 08:42:45 -0800

    Update mappingreads.sh

--- mappingreads.sh
+++ mappingreads.sh
@@ -10,12 +10,16 @@
 
 module load module load bwa/0.7.16a
 
+#if not indexed: make index of reference
+#bwa index Arabidopsis_thaliana.TAIR10.dna.toplevel.fa
 
 cat runIDs_fordl.txt | while read line; do
   NAME=$line
   bwa mem \
-  Arabidopsis_thaliana.TAIR10.dna.toplevel.fa\
-  $NAME"_forward_paired.fq.gz \
+  -t 10 \
+  -R "@RG\tID:Athal_Analyses\tSM:"$NAME"\tPL:ILLUMINA" \
+  Arabidopsis_thaliana.TAIR10.dna.toplevel.fa \
+  "$NAME"_forward_paired.fq.gz \
   "$NAME"_reverse_paired.fq.gz \
   > "$NAME"_alns.sam
 done
