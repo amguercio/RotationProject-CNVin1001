b40660780b4dcd962b5c95f6f0c101f8b3ba4cd3
amguercio <45185200+amguercio@users.noreply.github.com>
2018-12-11 10:19:40 -0800

    Delete plotting_mappedbins.R

--- plotting_mappedbins.R
+++
@@ -1,8 +0,0 @@
-#plotting_mappedbins.R
-
-##Script to run the Comai lab bin-by-sam script to plot read mapping densities
-
-## amguercio (Angelica Guercio) 2018
-
-
-##Script to plot bin-by-sam results to visualize read coverage/ chromosomal region
