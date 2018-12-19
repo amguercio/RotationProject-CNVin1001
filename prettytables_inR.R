a40cc827d54522380519d225d7419973a3149f16
amguercio <45185200+amguercio@users.noreply.github.com>
2018-12-19 12:13:17 -0800

    Update prettytables_inR.R

--- prettytables_inR.R
+++ prettytables_inR.R
@@ -1,33 +1,37 @@
-#prettytables_inR.R
+#####################
+#################################################
+#Script Name:         prettytables_inR.R
+#Description:         Script to make a pretty table from a csv (for publications etc)
+#Author:              amguercio (Angelica Guercio) 2018
+#################################################
+######################
 
-##Make a pretty table from a csv
 
-## amguercio (Angelica Guercio) 2018
 
- 
+#################################################
+###Loading libraries 
 
 #install.packages("formattable") 
-
-
-#Loading libraries 
 library(formattable) 
 library(ggplot2)
 library(gridExtra)
 
 
+#################################################
 #Read in the data
 tab = read.table("data_foranalyses.csv", header=F, sep=',', stringsAsFactors=F) 
+
 
 #Add column names 
 colnames(tab) = c("SampleID", "Name", "CS_Number", "Country", "Lat", 
                          "Lon", "Researcher", "Institution", "SRR_Number", "SE/PE")
 
 
-#create the actual table
+#################################################
+#Create the actual table
 mytable = formattable(tab) 
 #display the table in your web browser or in RStudio (if you use it) 
-mytable 
-
+#mytable 
 
 
 ##use ggplot to export table 
