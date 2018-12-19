bc1315a89e88a596e8c70dda9c6e19bf57cf6bed
amguercio <45185200+amguercio@users.noreply.github.com>
2018-12-19 11:49:56 -0800

    Update getting_ENAftps.py

--- getting_ENAftps.py
+++ getting_ENAftps.py
@@ -1,19 +1,20 @@
-#getting_ENAftps.py
+#####################
+#################################################
+#Script Name:         getting_ENAftps.py
+#Description:         Script using runIDs to build download link for downloading sequences
+#Author:              amguercio (Angelica Guercio) 2018
+#################################################
+######################
 
-##Script to get run IDs in order to download reads files randomly chosen from 50 accessions from the arabidopsis 1001 genomes project
 
-## amguercio (Angelica Guercio) 2018
-
-
-
-
+#################################################
 ##To download the files from ENA (DON'T EVEN TRY WITH THE SRA BS--SERIOUSLY)
 ##Site = https://www.ebi.ac.uk/ena/data/view/PRJNA273563
 ##Project # = PRJNA273563
 
 
 #Using the 'run IDs' from runIDs_fordl.txt (our subsetted samples), we can 'build' the ftp address from ena and add wget to the line
-#then we can just run this resulting script to make it wget all of our files!
+#then we can just run this resulting output file (as a script) to wget all of our files!
 
 #some important info about how ena ftp addresses are organized can be found here under 'Fastq file directory organisation':
 #https://www.ebi.ac.uk/ena/browse/read-download#downloading_files_ena_browser
@@ -28,14 +29,13 @@
 
 #all 1001 accessions have 7 digits-- so will be in 001 format
 
-#Here is the python parser to take the runIDs and make them into wget-able ftp addresses
 
 
+#################################################
+###Here is the python parser to take the runIDs and make them into wget-able ftp addresses
 
 Infile = open("runIDs_fordl.txt", "r")
-
 Outfile = open("download_ftps.sh", "w")
-
 
 
 for line in Infile:
@@ -45,8 +45,18 @@ for line in Infile:
         first3dig = parts[3] + parts[4] + parts[5]    #get the first 3 digits for <dir1>
         lastdig = parts[9]         #get the last digit for <dir2>
         Outfile.write("wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR" + first3dig + "/" + "00" + lastdig + "/" + fullname + "/" + fullname + "_1.fastq.gz" + "\n")
+        #for the PE _1.fastq.gz file
         Outfile.write("wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR" + first3dig + "/" + "00" + lastdig + "/" + fullname + "/" + fullname + "_2.fastq.gz" + "\n")
+        #for the PE _2.fastq.gz file
+
 Infile.close()
 Outfile.close()
+
+
+###The resulting file will look like this:
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR194/008/SRR1945948/SRR1945948_1.fastq.gz
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR194/008/SRR1945948/SRR1945948_2.fastq.gz
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR194/007/SRR1945627/SRR1945627_1.fastq.gz
+#...
 
 
