68522459d99c7de14fd72d14b6561a0ab6888f5e
amguercio <45185200+amguercio@users.noreply.github.com>
2018-12-19 12:04:47 -0800

    Update bin-by-sam_runscript.sh

--- bin-by-sam_runscript.sh
+++ bin-by-sam_runscript.sh
@@ -1,14 +1,19 @@
-#bin-by-sam_runscript.sh
+#####################
+#################################################
+#Script Name:         bin-by-sam_runscript.sh
+#Description:         Script to run the Comai lab bin-by-sam script to plot read mapping densities
+#Author:              amguercio (Angelica Guercio) 2018
+#################################################
+######################
 
-##Script to run the Comai lab bin-by-sam script to plot read mapping densities
 
-## amguercio (Angelica Guercio) 2018
-
-
+#################################################
 ##documentation + dl page http://comailab.genomecenter.ucdavis.edu/index.php/Bin-by-sam
 ##detailed map page http://comailab.genomecenter.ucdavis.edu/images/3/30/README-bin-by-sam.pdf
 
-
+#################################################
 ##command to run:
 
 python bin-by-sam_2.0.py -o binnedmaps.txt -s 100000 
+#-o flag indicates the outfile to save to 
+#-s flag indicates 100kb bin size
